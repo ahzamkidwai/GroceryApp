290cb0f89a041faf3fab6202c4a2ee42057af13c
ahzamkidwai <ahzamnaseem.kidwai@gmail.com>
2025-04-21 13:28:18 +0530

    Create Order Schema

---
+++ server/src/models/index.js
@@ -0,0 +1,17 @@
+import { Admin, Customer, DeliveryPartner } from "./user.js";
+import Branch from "./branch.js";
+import Counter from "./counter.js";
+import Order from "./order.js";
+import Product from "./products.js";
+import Category from "./Category.js";
+
+export {
+  Admin,
+  Customer,
+  DeliveryPartner,
+  Counter,
+  Order,
+  Branch,
+  Product,
+  Category,
+};
