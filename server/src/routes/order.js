e3e6af779cef880790f73407cc69c59a9acdbd6c
ahzamkidwai <ahzamnaseem.kidwai@gmail.com>
2025-04-22 11:47:03 +0530

    Add order routes

---
+++ server/src/routes/order.js
@@ -0,0 +1,22 @@
+import { verifyToken } from "../middlewares/auth";
+import {
+  createOrder,
+  confirmOrder,
+  updateOrderStatus,
+  getOrders,
+  getOrderById,
+} from "../controllers/order/order.js";
+
+export const orderRoutes = async (fastify, options) => {
+  fastify.addHook("preHandler", async (request, reply) => {
+    const isAuthenticated = await verifyToken(request, reply);
+    if (!isAuthenticated) {
+      return reply.status(401).send({ message: "Unauthorized jwt token" });
+    }
+  });
+  fastify.post("/order", createOrder);
+  fastify.get("/order", getOrders);
+  fastify.patch("/order/:orderId/status", updateOrderStatus);
+  fastify.post("/order/:orderId/confirm", confirmOrder);
+  fastify.get("/order/:orderId", getOrderById);
+};
