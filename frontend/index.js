6507900f37ee62df225cbd129bdb57adab070539
ahzamkidwai <ahzamnaseem.kidwai@gmail.com>
2025-04-25 12:20:28 +0530

    Add small code to remove react-native-reanimated warnings and error

--- frontend/index.js
+++ frontend/index.js
@@ -6,4 +6,16 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
+// To avoid react-native-reanimated warnings and errors
+
+import {
+  configureReanimatedLogger,
+  ReanimatedLogLevel,
+} from 'react-native-reanimated';
+
+configureReanimatedLogger({
+  level: ReanimatedLogLevel.warn,
+  strict: false,
+});
+
 AppRegistry.registerComponent(appName, () => App);
